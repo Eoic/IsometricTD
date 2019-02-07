e98cf1428d99b481883451b329d732961ffe8232
Karolis <karolis.stradzdas00@gmail.com>
2019-02-08 00:33:21 +0200

    Add resources prop object

---
+++ Assets/Scripts/Resource.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resource : MonoBehaviour
+{
+    public enum ResourceType { STONE, IRON, WOOD }
+    [SerializeField] private ResourceType type = ResourceType.STONE;
+    [SerializeField] private int supply = 100;
+
+    public ResourceType Type { get => type; }
+    public int Supply { get => supply; }
+    public bool IsDepleted { get => (supply <= 0); }
+
+    public int ConsumeResource(int amount)
+    {
+        if (supply - amount < 0)
+        {
+            int consumed = supply;
+            supply = 0;
+            return consumed;
+        }
+
+        supply -= amount;
+        return amount;
+    }
+}
--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -4,15 +4,79 @@ using UnityEngine;
 
 public class ResourceManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static ResourceManager instance = null;
+    public int Stone { get => stone; }
+    public int Wood { get => wood; }
+    public int Iron { get => iron; }
+
+    [SerializeField] private int stone;
+    [SerializeField] private int wood;
+    [SerializeField] private int iron;
+
+    private void Awake()
     {
-        
+        if (instance == null)
+            instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Inc.
+    public void AddStone(int amount)
     {
-        
+        if (!IsValueValid(amount))
+            return;
+
+        stone += amount;
+    }
+
+    public void AddWood(int amount)
+    {
+        if (!IsValueValid(amount))
+            return;
+
+        wood += amount;
+    }
+
+    public void AddIron(int amount)
+    {
+        if (!IsValueValid(amount))
+            return;
+
+        iron += amount;
+    }
+
+    //Dec.
+    public bool ConsumeStone(int amount)
+    {
+        if (stone - amount < 0)
+            return false;
+
+        stone -= amount;
+        return true;
+    }
+
+    public bool ConsumeWood(int amount)
+    {
+        if (stone - amount < 0)
+            return false;
+
+        stone -= amount;
+        return true;
+    }
+
+    public bool ConsumeIron(int amount)
+    {
+        if (stone - amount < 0)
+            return false;
+
+        stone -= amount;
+        return true;
+    }
+
+    private bool IsValueValid(int amount)
+    {
+        if (amount <= 0)
+            return false;
+
+        return true;
     }
 }
