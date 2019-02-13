282ebfad9ad9a22424778449051671ab6df0e714
Karolis <karolis.stradzdas00@gmail.com>
2019-02-13 23:37:50 +0200

    Add minimap

---
+++ Assets/Scripts/MinimapFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapFollow : MonoBehaviour
+{
+    public Transform worldCamera;
+
+    void LateUpdate()
+    {
+        Vector3 minimapPosition = worldCamera.position;
+        minimapPosition.y = transform.position.y;
+        transform.position = minimapPosition;
+    }
+}
