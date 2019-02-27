dcd6f2c6e4ff5c9861da231487deaad1ab895209
Eoic <karolis.strazdas@ktu.edu>
2019-02-27 19:44:13 +0200

    Slightly change terrain color

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -8,18 +8,14 @@ public class Building : MonoBehaviour
     private Renderer objectRenderer;
     [SerializeField] private Constants.Buildings buildingId;
     [SerializeField] private string buildingName;
-    [SerializeField] private int hitPoints;
     [SerializeField] private int stoneCost;
     [SerializeField] private int woodCost;
     [SerializeField] private int ironCost;
-    [SerializeField] private int hitPointsCurrent;
-    [SerializeField] private bool isDestroyed;
     [SerializeField] private bool onValidPosition;
     [SerializeField] private bool isBuilt = false;
     
     public Constants.Buildings BuildingId { get => buildingId; set => buildingId = value; }
     public string BuildingName { get => buildingName; }
-    public int HitPoints { get => hitPoints; }
     public int StoneCost { get => stoneCost; }
     public int WoodCost { get => woodCost; }
     public int IronCost { get => ironCost; }
