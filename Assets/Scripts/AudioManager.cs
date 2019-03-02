0e5d87d6c500d3427eb229fb89dc7341528cd8e2
tomas962 <cctomass@gmail.com>
2019-03-02 18:50:03 +0200

    Added AudioManager for music and other UI sounds, TorchFire sound effect and theme music

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (var s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+
+    void Start()
+    {
+        Play("Theme1");
+    }
+
+    public void Play(string name)
+    {
+        foreach (var s in sounds)
+        {
+            if (s.name == name)
+            {
+                s.source.Play();
+                return;
+            }
+        }
+        Debug.LogWarning("Sound: " + name + " not found!");
+    }
+}
