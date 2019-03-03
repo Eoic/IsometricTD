c9f606309ebfb76cdac67bd8fe94f83af22fd142
Eoic <karolis.strazdas@ktu.edu>
2019-03-04 00:39:00 +0200

    Trees

--- Assets/Scripts/Generators/TreePlanter.cs
+++ Assets/Scripts/Generators/TreePlanter.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
+using UnityEditor;
 
 [ExecuteInEditMode]
 public class TreePlanter : MonoBehaviour
 {   
     public GameObject[] resources;
-
+    
     [Header("Rotation")]
     public bool randomRotation;
     [Range(1, 360)] public int maxRotation = 1;
@@ -12,7 +13,9 @@ public class TreePlanter : MonoBehaviour
     [Header("Scale")]
     public bool randomScale;
     [Range(1, 10)]          public int maxScale = 1;
-    [Range(0.001f, 0.02f)]  public double densityTreshold = 0.01;
+
+    [Header("Density")]
+    public double densityTreshold = 0.01;
     
     [Header("Size")]
     [Min(1)]        public int mapWidth = 10;
@@ -36,11 +39,11 @@ public class TreePlanter : MonoBehaviour
             {
                 if(mapMatrix[i, j] == 1)
                 {
-                    GameObject asset = Instantiate(PickRandomTree());
-                    asset.transform.position = new Vector3(i, asset.transform.position.y, j);
+                    GameObject asset = PrefabUtility.InstantiatePrefab(PickRandomTree()) as GameObject;
+                    asset.transform.position = new Vector3(i + transform.position.x, asset.transform.position.y, j + transform.position.z);
                     asset.transform.parent = transform;
-                    ApplyRotation(asset);
-                    ApplyScale(asset);
+                    //ApplyRotation(asset);
+                    //ApplyScale(asset);
                     generatedCount++;
                 }
             }
@@ -51,6 +54,11 @@ public class TreePlanter : MonoBehaviour
     
     public void GeneratePlantPoints()
     {
+        if (densityTreshold > 1)
+            densityTreshold = 1;
+        else if (densityTreshold < 0)
+            densityTreshold = 0;
+
         for(int i = 0; i < mapMatrix.GetLength(0); i++)
             for(int j = 0; j < mapMatrix.GetLength(1); j++)
                 mapMatrix[i, j] = random.NextDouble() >= densityTreshold ? 0 : 1;
