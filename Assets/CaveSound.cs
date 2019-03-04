bb77a6bc9309c7264225c7ba1244f4fd7a2ca7ae
tomas962 <cctomass@gmail.com>
2019-03-04 19:58:02 +0200

    Building menu click sounds, building sound, sound of monsters in the caves. Also added various UI audio samples.

---
+++ Assets/CaveSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveSound : MonoBehaviour
+{
+    AudioSource source;
+    public bool playMonsterSound = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("script started");
+        source = GetComponent<AudioSource>();
+        InvokeRepeating("Playmonstersound", 3f, 30f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!playMonsterSound)
+            CancelInvoke();
+    }
+
+    void Playmonstersound()
+    {
+        source.Play();
+    }
+}
