ef441a814b7827685e9656d57b46c3f17e21f791
tomas962 <cctomass@gmail.com>
2019-03-04 22:40:52 +0200

    Changes

--- Assets/Scripts/RiverSounds.cs
+++ Assets/Scripts/RiverSounds.cs
@@ -7,15 +7,17 @@ public class RiverSounds : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        var parentClip = GetComponent<AudioSource>();
         for (int i = 0; i < transform.childCount; i++)
         {
+            
             var riverTile = transform.GetChild(i);
             riverTile.gameObject.AddComponent<AudioSource>();
             var audioSource = riverTile.GetComponent<AudioSource>();
-            audioSource.clip = GetComponent<AudioSource>().clip;
-            audioSource.volume = GetComponent<AudioSource>().volume;
-            audioSource.spatialBlend = 1;
-            audioSource.loop = true;
+            audioSource.clip = parentClip.clip;
+            audioSource.volume = parentClip.volume;
+            audioSource.spatialBlend = parentClip.spatialBlend;
+            audioSource.loop = parentClip.loop;
             audioSource.Play();
         }
         
