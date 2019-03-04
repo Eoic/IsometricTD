1e27db1ba55e48f2eaf11f50f09125618590228f
tomas962 <cctomass@gmail.com>
2019-03-04 20:42:45 +0200

    River sounds

---
+++ Assets/RiverSounds.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiverSounds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var riverTile = transform.GetChild(i);
+            riverTile.gameObject.AddComponent<AudioSource>();
+            var audioSource = riverTile.GetComponent<AudioSource>();
+            audioSource.clip = GetComponent<AudioSource>().clip;
+            audioSource.volume = GetComponent<AudioSource>().volume;
+            audioSource.spatialBlend = 1;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+        
+    }
+
+}
