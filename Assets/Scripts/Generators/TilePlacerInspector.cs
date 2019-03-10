65b0cc190e5aa28a0cbc1f5e78a603165f1d4903
Eoic <karolis.strazdas@ktu.edu>
2019-03-10 17:26:52 +0200

    Map

---
+++ Assets/Scripts/Generators/TilePlacerInspector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TilePlacer))]
+public class TilePlacerInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        TilePlacer generator = (TilePlacer)target;
+
+        if (GUILayout.Button("Place"))
+            generator.Place();
+    }
+}
