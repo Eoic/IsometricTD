b3ca4b99e303833451c2499706867aafe77ae105
Eoic <karolis.strazdas@ktu.edu>
2019-03-11 18:38:04 +0200

    :(

---
+++ Assets/Scripts/MapSwitch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSwitch : MonoBehaviour
+{
+    private int focusIndex;
+    private bool transitionComplete;
+    public Vector3[] cameraFocusPoints;
+    public GameObject worldCamera;
+
+    void Start()
+    {
+        focusIndex = 0;
+        transitionComplete = false;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            focusIndex = (focusIndex + 1) % cameraFocusPoints.Length;
+            worldCamera.transform.position = cameraFocusPoints[focusIndex];
+        }
+    }
+}
