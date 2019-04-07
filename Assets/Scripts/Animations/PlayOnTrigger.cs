44059fc0bf3ba9afc1fa235dc8c78da01a171077
Eoic <karolis.strazdas@ktu.edu>
2019-04-07 18:03:09 +0100

    Add gate animation on physics trigger

---
+++ Assets/Scripts/Animations/PlayOnTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayOnTrigger : MonoBehaviour
+{
+    public Animator gateAnimator;
+    public bool gateClosed = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (gateAnimator != null && !gateClosed)
+        {
+            Debug.Log("Triggered0");
+            gateAnimator.Play("GateClosing");
+            gateClosed = true;
+        }
+    }
+}
