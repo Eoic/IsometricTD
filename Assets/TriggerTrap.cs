ea3f844600738a1e57eb2f5babce19c05db5f42a
Eoic <karolis.strazdas@ktu.edu>
2019-04-09 01:21:51 +0100

    :{

---
+++ Assets/TriggerTrap.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTrap : MonoBehaviour
+{
+    public Animator animator;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.Play("TrapRelease");
+    }
+}
